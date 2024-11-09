2cf5f116fee6e8fc4e7a27d3346de67d07e87373
Xateh <155933957+Xateh@users.noreply.github.com>
2024-11-09 10:39:42 +0800

    Update smallest_bounding_rectangle.js

--- smallest_bounding_rectangle.js
+++ smallest_bounding_rectangle.js
@@ -1,3 +1,8 @@
+const get_x = rs => list_ref(rs, 0);
+const get_y = rs => list_ref(rs, 1);
+const get_width = rs => list_ref(rs, 2);
+const get_height = rs => list_ref(rs, 3);
+
 function smallest_bounding_AAR_area(rs) {
 
     // WRITE YOUR SOLUTION HERE.
@@ -7,18 +12,10 @@ function smallest_bounding_AAR_area(rs) {
                 (x, ys) => is_null(ys)
                             ? x
                             : list(
-                                get_x(x) < get_x(ys)
-                                ? get_x(x)
-                                : get_x(ys),
-                                get_y(x) < get_y(ys)
-                                ? get_y(x)
-                                : get_y(ys),
-                                get_width(x) > get_width(ys)
-                                ? get_width(x)
-                                : get_width(ys),
-                                get_height(x) > get_height(ys)
-                                ? get_height(x)
-                                : get_height(ys)
+                                math_min(get_x(x), get_x(ys)),
+                                math_min(get_y(x), get_y(ys)),
+                                math_max(get_width(x), get_width(ys)),
+                                math_max(get_height(x), get_height(ys))
                                 ),
                 null,
                 map(x => list(
@@ -38,14 +35,14 @@ function optimized_smallest_bounding_AAR_area(rs) {
 
     // WRITE YOUR SOLUTION HERE.
     return (x => head(x) * tail(x))
-        (accumulate(
-            (x, ys) => is_null(ys)
-                        ? pair(math_max(get_width(x), get_height(x)),
-                                math_min(get_width(x), get_height(x)))
-                        : pair(math_max(math_max(get_width(x), get_height(x)), head(ys)),
-                                math_max(math_min(get_width(x),get_height(x)), tail(ys))),
-            null,
-            rs));
+            (accumulate(
+                (x, ys) => is_null(ys)
+                            ? pair(math_max(get_width(x), get_height(x)),
+                                    math_min(get_width(x), get_height(x)))
+                            : pair(math_max(math_max(get_width(x), get_height(x)), head(ys)),
+                                    math_max(math_min(get_width(x),get_height(x)), tail(ys))),
+                null,
+                rs));
 }
 
 // const aar1 = list(2, 3, 10, 15);
@@ -56,24 +53,14 @@ function optimized_smallest_bounding_AAR_area(rs) {
 function overlap_area(aar1, aar2) {
 
     // WRITE YOUR SOLUTION HERE.
-    return (get_x(aar1) <= get_x(aar2) + get_width(aar2) &&
-            get_x(aar1) >= get_x(aar2)
-            ? math_min(get_x(aar2) + get_width(aar2) - get_x(aar1),
-                get_width(aar1))
-            : get_x(aar2) <= get_x(aar1) + get_width(aar1) &&
-            get_x(aar2) >= get_x(aar1)
-            ? math_min(get_x(aar1) + get_width(aar1) - get_x(aar2),
-                get_width(aar2))
-            : 0) * 
-            (get_y(aar1) <= get_y(aar2) + get_height(aar2) &&
-            get_y(aar1) >= get_y(aar2)
-            ? math_min(get_y(aar2) + get_height(aar2) - get_y(aar1),
-                get_height(aar1))
-            : get_y(aar2) <= get_y(aar1) + get_height(aar1) &&
-            get_y(aar2) >= get_y(aar1)
-            ? math_min(get_y(aar1) + get_height(aar1) - get_y(aar2),
-                get_height(aar2))
-            : 0);
+    const l_limit = math_max(get_x(aar1), get_x(aar2));
+    const r_limit = math_min(get_x(aar1) + get_width(aar1),
+                            get_x(aar2) + get_width(aar2));
+    const b_limit = math_max(get_y(aar1), get_y(aar2));
+    const t_limit = math_min(get_y(aar1) + get_height(aar1),
+                            get_y(aar2) + get_height(aar2));
+    
+    return math_max(r_limit - l_limit, 0) * math_max(t_limit - b_limit, 0);
 }
 
 // overlap_area( list(10, 20, 30, 60), list(40, 10, 50, 15) );
